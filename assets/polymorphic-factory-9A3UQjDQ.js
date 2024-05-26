cba1a0cf6052c98ad7df5faa425f32e5b29b2d63
qinghaoh <6961882+qinghaoh@users.noreply.github.com>
2024-05-25 21:15:29 -0700

    Updates

---
+++ assets/polymorphic-factory-9A3UQjDQ.js
@@ -0,0 +1,1 @@
+import{r as t,D as e}from"./index-CqJwE9tc.js";function p(r){const o=t.forwardRef(r);return o.extend=e,o}export{p};
--- assets/polymorphic-factory-tmW0cuKC.js
+++
@@ -1,1 +0,0 @@
-import{r as t,D as e}from"./index-CP4h7CKO.js";function p(r){const o=t.forwardRef(r);return o.extend=e,o}export{p};
